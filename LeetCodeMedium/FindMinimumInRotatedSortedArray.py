b2bf3fdd0f4def4e7ced7d82d13c5f21909e7468
Arun Aaryan <rnyadav98@gmail.com>
2021-05-14 14:21:36 +0530

    Made some corrections

--- LeetCodeMedium/FindMinimumInRotatedSortedArray.py
+++ LeetCodeMedium/FindMinimumInRotatedSortedArray.py
@@ -6,15 +6,18 @@ class Solution:
         left = 0
         right = len(nums) - 1
         while left < right:
+            if nums[left] < nums[mid]:
+                return nums[left]
             mid = (left + right) // 2
-            if nums[mid]  > nums[mid + 1]:
-                right = mid 
+            if nums[mid]  >= nums[left]:
+                left = mid + 1 
             else:
-                left = mid + 1
-        return left   
+                right = mid
+        return nums[left]
     
 s = Solution()
-res = s.findMin([1,2,1,3,5,6,4])
+# res = s.findMin([1,2,1,3,5,6,4])
+res = s.findMin([3,4,5,1,2])
 print(res)
 
 
