29fd858ccb9d776d1cd7d90773cbd6b9f0201d7e
arunaaryan <rnyadav98@gmail.com>
2021-08-18 08:36:45 +0530

    day79

--- LeetCodeHard/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-            while nums[current] > 0 and nums[current] <= n and nums[nums[current] - 1] != nums[current]:
-                nums[nums[current] - 1], nums[current] = nums[current], nums[nums[current]]
