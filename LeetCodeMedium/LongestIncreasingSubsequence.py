505191d90b74db08ad9920c5874a990aac6d2410
arunaaryan <rnyadav98@gmail.com>
2022-03-09 11:12:45 +0530

    day112

--- LeetCodeMedium/LongestIncreasingSubsequence.py
+++ LeetCodeMedium/LongestIncreasingSubsequence.py
@@ -1,16 +1,16 @@
 from typing import List
+
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         lis = [1] * len(nums)
-        for i in range(len(nums) -1, -1 ,-1):
+        for i in range(len(nums) - 1, -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[i] < nums[j]:
                     lis[i] = max(lis[i], lis[j] + 1)
         return max(lis)
 
 
-    
-        
 s = Solution()
-res =  s.lengthOfLIS([10,9,2,5,3,7,101,18])
+res = s.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])
 print(res)
--- LeetCodeMedium/PacificAtlantic.py
+++ LeetCodeMedium/PacificAtlantic.py
@@ -1,23 +1,26 @@
+from collections import defaultdict, deque
 from collections import deque
+
+
 class Solution:
     def pacificAtlantic(self, heights):
         if not heights or not heights[0]:
             return []
         rows = len(heights)
         cols = len(heights[0])
-        pacific_coords = deque() 
+        pacific_coords = deque()
         atlantic_coords = deque()
         for i in range(rows):
             pacific_coords.append((i, 0))
-            atlantic_coords.append((i, cols -1))
-        
+            atlantic_coords.append((i, cols - 1))
+
         for i in range(cols):
             pacific_coords.append((0, i))
             atlantic_coords.append((rows-1, i))
-        
+
         def bfs(queue):
             visited = set()
-            directions = [(1,0),(-1,0),(0,-1),(0,1)]
+            directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
             while queue:
                 row, col = queue.popleft()
                 visited.add((row, col))
@@ -47,7 +50,7 @@ class Solution:
 
         def dfs(row, col, reachable):
             reachable.add((row, col))
-            directions = [(1,0),(-1,0),(0,1),(0,-1)]
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
             for (x, y) in directions:
                 adj_row = row + x
                 adj_col = col + y
@@ -56,21 +59,63 @@ class Solution:
                 if (adj_row, adj_col) in reachable:
                     continue
                 if heights[adj_row][adj_col] >= heights[row][col]:
-                    dfs(adj_row,adj_col, reachable)
-        
+                    dfs(adj_row, adj_col, reachable)
+
         for i in range(rows):
             dfs(i, 0, pacific_reachable)
             dfs(i, cols-1, atlantic_reachable)
-        
+
         for i in range(cols):
             dfs(0, i, pacific_reachable)
-            dfs(rows -1, i, atlantic_reachable)
+            dfs(rows - 1, i, atlantic_reachable)
 
         return list(pacific_reachable.intersection(atlantic_reachable))
 
-            
+
+# one of the best solutions from leetcode submissions
+
+DIRECTIONS = ((0, 1), (1, 0), (0, -1), (-1, 0))
+PACIFIC = 1
+ATLANTIC = 2
+
+
+class Solution:
+    def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+        flow = defaultdict(set)
+        rows = len(matrix)
+        if not rows:
+            return []
+        cols = len(matrix[0])
+        queue = deque()
+        for row in range(rows):
+            queue.append((row, 0, PACIFIC))
+            queue.append((row, cols - 1, ATLANTIC))
+
+        for col in range(cols):
+            queue.append((0, col, PACIFIC))
+            queue.append((rows - 1, col, ATLANTIC))
+
+        while queue:
+            row, col, sea = queue.popleft()
+            flow[(row, col)].add(sea)
+
+            for dr, dc in DIRECTIONS:
+                r1 = dr + row
+                c1 = dc + col
+                if r1 >= 0 and c1 >= 0 and r1 < rows and c1 < cols and matrix[row][col] <= matrix[r1][c1] and sea not in flow[(r1, c1)]:
+                    queue.append((r1, c1, sea))
+
+        flow((3, 1): [1, 2])
+        flowboth = []
+        for key, seas in flow.items():
+            if len(seas) == 2:
+                flowboth.append(key)
+
+        return flowboth
+
+
 s = Solution()
 # res = s.pacificAtlantic([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]])
-res = s.pacificAtlanticDfs([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]])
+res = s.pacificAtlanticDfs([[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [
+                           2, 4, 5, 3, 1], [6, 7, 1, 4, 5], [5, 1, 1, 2, 4]])
 print(res)
-        
