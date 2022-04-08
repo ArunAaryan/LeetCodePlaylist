4b3610e81da37c54190083c1120991d9a542a48c
arunaaryan <rnyadav98@gmail.com>
2022-04-08 17:38:26 +0530

    make fixes in subset problems

--- LeetCodeMedium/Subsets.py
+++ LeetCodeMedium/Subsets.py
@@ -1,46 +1,49 @@
 from typing import List
+
+
 class Solution:
     def __init__(self):
-        self.st =  0
+        self.st = 0
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
         subset = []
         self.findSubset(nums, 0, res, subset)
         print(res)
+
     def findSubset(self, nums, idx, res, subset):
         if idx >= len(nums):
             res.append(subset.copy())
-            return 
+            return
         subset.append(nums[idx])
         self.findSubset(nums, idx+1, res, subset)
         subset.pop()
         self.findSubset(nums, idx+1, res, subset)
-        return  
+        return
 
-    #general solution for subsets
-    def subsets2(self,nums):
-        
-        nums.sort()
+    # general solution for subsets
+    def subsets2(self, nums):
+
+        # nums.sort() without sort also works
         def backtrack(res, templist, nums, start):
             self.st += 1
             res.append(templist.copy())
-            i = start 
-            while ( i < len(nums)):
+            i = start
+            while (i < len(nums)):
                 templist.append(nums[i])
                 backtrack(res, templist, nums, i+1)
                 templist.pop()
                 i += 1
 
         res = []
-        backtrack(res, [], nums, 0) 
-        return res 
-
+        backtrack(res, [], nums, 0)
+        return res
 
 
 s = Solution()
 # s.subsets([1,2,2])
-res = s.subsets2([3, 4,7,9])
+res = s.subsets2([3, 4, 7, 9])
 print(s.st)
 # print(res)
-#similar problem solutions in python
-#https://leetcode.com/problems/subsets/discuss/429534/General-Backtracking-questions-solutions-in-Python-for-reference-%3A
+# similar problem solutions in python
+# https://leetcode.com/problems/subsets/discuss/429534/General-Backtracking-questions-solutions-in-Python-for-reference-%3A
--- LeetCodeMedium/Subsets2.py
+++ LeetCodeMedium/Subsets2.py
@@ -8,27 +8,28 @@
 #     End For
 # If none of the move works out, return false, NO SOLUTON.
 from typing import List
+
+
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         def backtrack(nums, res, templist, start):
             res.append(templist.copy())
-            i = start 
+            i = start
             while i < len(nums):
-               if nums[i] == nums[i -1] and i > start:
-                   i += 1
-                   continue 
-               templist.append(nums[i])
-               backtrack(nums, res, templist, i + 1)
-               templist.pop()
-               i += 1
+                # handle the first element for 'list index out of range' 0 -1
+                if nums[i] == nums[i - 1] and i > 0:
+                    i += 1
+                    continue
+                templist.append(nums[i])
+                backtrack(nums, res, templist, i + 1)
+                templist.pop()
+                i += 1
         res = []
         nums.sort()
         backtrack(nums, res, [], 0)
         return res
-       
+
+
 s = Solution()
-res = s.subsetsWithDup([1,2,2])
+res = s.subsetsWithDup([1, 2, 2])
 print(res)
-
-
-        
