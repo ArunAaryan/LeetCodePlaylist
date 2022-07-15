d6303fb9c2e86533031c418120962b91bdf9dbd4
arunaaryan <rnyadav98@gmail.com>
2022-07-15 09:35:33 +0530

    BinaryTreeRightSideView Updated Solution

--- LeetCodeMedium/BinaryTreeRightSideView.py
+++ LeetCodeMedium/BinaryTreeRightSideView.py
@@ -23,6 +23,18 @@ class Solution:
                     q.append(node.right)
             res.append(last_val)
         return res
+    def rightSideViewDfs(self, root):
+        self.ans = []
+        
+        def dfs(root, depth):
+            if root == None: return
+            if depth == len(self.ans):  # When we meet this `depth` for the first time, let's add the first node as the right side most node.
+                self.ans.append(root.val)
+            dfs(root.right, depth + 1)  # Go right side first
+            dfs(root.left, depth + 1)
+            
+        dfs(root, 0)
+        return self.ans
 
             
         
