873a7f104fba401869f9086b8ee50c4143c953cf
arunaaryan <rnyadav98@gmail.com>
2022-09-27 10:31:25 +0530

    3sum closest updated

--- LeetCodeMedium/3SumClosest.py
+++ LeetCodeMedium/3SumClosest.py
@@ -2,19 +2,19 @@ class Solution:
     def threeSumClosest(self, nums, target):
         nums.sort()
         close_total = nums[0] + nums[1] + nums[2]
-        for first in range(len(nums) -2 ):
+        for first in range(len(nums) - 2 ):
             second = first + 1
             third = len(nums) - 1
             while second < third:
-                total = nums[first] + nums[second] + nums[third]
-                if   abs(total - target)< abs(close_total - target):
-                    close_total = total
-                if total < target:
+                current_total = nums[first] + nums[second] + nums[third]
+                if abs(current_total - target) < abs(close_total - target):
+                    close_total = current_total
+                if current_total < target:
                     second += 1
-                elif total > target:
+                elif current_total > target:
                     third -= 1
                 else:
-                    return total
+                    return current_total
             return close_total 
 s = Solution()
 res = s.threeSumClosest([-3,-2,-5,3,-4],-1)
