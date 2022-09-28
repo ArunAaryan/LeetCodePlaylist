dc161c0a5b87fafe4e9ce05b9851a9b76d823157
arunaaryan <rnyadav98@gmail.com>
2022-09-28 09:39:28 +0530

    SubArrayProduct Less than K

--- LeetCodeMedium/SubarrayProductLessThanK.py
+++ LeetCodeMedium/SubarrayProductLessThanK.py
@@ -12,3 +12,13 @@ class Solution:
                 left += 1
             ans += right - left + 1
         return ans
+    def similarSolution(self, nums, k):
+        start, end, prod, ans = 0, 0, 1, 0
+        while end < len(nums):
+            prod *= nums[end]
+            while end >= start and prod >= k:
+                prod /= nums[start]
+                start += 1
+            ans += end - start + 1
+            end += 1
+        return ans
