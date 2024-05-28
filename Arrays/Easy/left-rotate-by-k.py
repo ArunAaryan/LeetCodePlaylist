0998bb474806f1889f03c844cfbce56f38a8881c
ArunAaryan <rnyadav98@gmail.com>
2024-05-28 07:19:18 +0530

    add new solution

--- Arrays/Easy/left-rotate-by-k.py
+++ Arrays/Easy/left-rotate-by-k.py
@@ -1,17 +1,37 @@
 arr = [1, 2, 3, 4, 5, 6]
+# arr= [6, 5, 4, 3, 2, 1]
+# arr = [3, 4, 5, 6, 1, 2]
+# arr = [3, 4, 5, 6, 1, 2]
 k = 2
-def rotate(arr, k):
-    k = k % len(arr)
-    print(k)
-    if len(arr) == 1 or len(arr) == k:
-        return arr
-    temp = []
-    for i in range(k):
-        temp.append(arr[i])
-    for j in range(len(arr) - k):
-        arr[j] = arr[j + k]
-    for i in range(len(arr) - k, len(arr)):
-        arr[i] = temp[i - k - 1]
+def _rotate(arr):
+    temp = arr[0]
+    for i in range(1, len(arr)):
+        arr[i - 1] = arr[i]
+    arr[i] = temp 
 
-rotate(arr, k)
+def rotate(arr, k):
+    r = k % len(arr)
+    if r == len(arr):
+        return arr 
+    for _ in range(r):
+        _rotate(arr)
+    
+
+
+# rotate(arr, k)
+# print(arr)
+def rotateReverse_(arr, low, high):
+    while low < high:
+        arr[low], arr[high] = arr[high], arr[low]
+        low += 1
+        high -= 1
+    
+def rotateReverse(arr, k):
+    k %= len(arr)
+    if len(arr) == k:
+        return arr 
+    rotateReverse_(arr, 0, len(arr) - 1)
+    rotateReverse_(arr, 0,  k - 1)
+    rotateReverse_(arr, k ,  len(arr) - 1)
+rotateReverse(arr, k)
 print(arr)
