37f5411e8597228bd9713720cf010b7ab7da2848
ArunAaryan <rnyadav98@gmail.com>
2024-06-19 21:05:21 +0530

    Add new solution

--- LeetCodeEasy/MajorityElement.py
+++ LeetCodeEasy/MajorityElement.py
@@ -1,7 +1,22 @@
 def majorityElement(arr):
     arr.sort()
     print(arr)
-    return arr[len(arr)//2]
+    return arr[len(arr) // 2]
 
-    
-print(majorityElement([1,2,1,4,2,4,5,2]))
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # moore's voting algorithm
+        candidate, votes = nums[0], 0
+        for i in range(len(nums)):
+            if not votes:
+                candidate = nums[i]
+            if nums[i] == candidate:
+                votes += 1
+            else:
+                votes -= 1
+
+        return candidate
+
+
+print(majorityElement([1, 2, 1, 4, 2, 4, 5, 2]))
