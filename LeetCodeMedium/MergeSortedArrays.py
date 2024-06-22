3627f3c0ca3868bece90cb00de87470bc9ae3857
ArunAaryan <rnyadav98@gmail.com>
2024-06-22 14:37:45 +0530

    MergeSortedArrays.py

---
+++ LeetCodeMedium/MergeSortedArrays.py
@@ -0,0 +1,31 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        a, b, write_index = m - 1, n - 1, m + n - 1
+
+        while b >= 0:
+            if a >= 0 and nums1[a] > nums2[b]:
+                nums1[write_index] = nums1[a]
+                a -= 1
+            else:
+                nums1[write_index] = nums2[b]
+                b -= 1
+
+            write_index -= 1
+
+
+# solution 2 early exit and copy elements to be much faster in some scenarios where more greater
+# elements are on first array
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i, j = m - 1, n - 1
+        k = m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        if j >= 0:
+            nums1[: j + 1] = nums2[: j + 1]
